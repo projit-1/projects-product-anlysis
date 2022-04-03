be8eee4051ed1911604be5cc07c193f95bc78993
projit-1 <projitmondol@gmail.com>
2022-04-03 23:49:38 +0600

    hook component is added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import bmwx6 from '../../image/bmwx6.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h3>home</h3>
+            <div className="main-container">
+                <div className="body">
+                    <h1>
+                        The Car You Want Next !!
+                    </h1>
+
+                    <p>High quality finishes, incredible and intuitive interface, tight build, sports car handling with SUV ride height. 5L v8 turbo has power to spare and then some. Don’t even need Sport mode but wow! it’s fun when you turn it on.</p>
+
+                    <button>Live Demo</button>
+                </div>
+                <div className="product-pic">
+                    <img src={bmwx6} alt="" />
+                </div>
+            </div>
+            <div className="customer-reviews">
+                <h2>Customers Reviews</h2>
+            </div>
+            <div className="all-reviews">
+                <button>See All Reviews</button>
+            </div>
         </div>
     );
 };
