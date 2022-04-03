489ef0affc95ffe37bcad37c1b90028fe2f2eb2a
projit-1 <projitmondol@gmail.com>
2022-04-03 21:59:22 +0600

    git is added

---
+++ src/Components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h4>blooooogs</h4>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Components/Reviews/Reviwes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviwes = () => {
+    return (
+        <div>
+            <h3> this ia reviews</h3>
+        </div>
+    );
+};
+
+export default Reviwes;
