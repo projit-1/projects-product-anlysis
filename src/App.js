23d05f949c603d625757d33172031e0177f59ea2
projit-1 <projitmondol@gmail.com>
2022-04-03 22:52:59 +0600

    initially all components are added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Reviwes from './Components/Reviews/Reviwes';
 
 function App() {
   return (
     <div className="App">
-      <h4>okkkk</h4>
-    </div>
+      <Header></Header>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/Reviews' element={<Reviwes></Reviwes>} ></Route>
+        <Route path='/Dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/Blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+
+      </Routes>
+    </div >
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../image/logo.png'
+import './Header.css'
 const Header = () => {
     return (
         <div>
-            <h3>header</h3>
+            <div className="navbar">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="menu">
+                    <Link to="/home">HOME</Link>
+                    <Link to="/Reviews">REVIEWS</Link>
+                    <Link to="Dashboard">DASHBOARD</Link>
+                    <Link to="/Blogs">BLOGS</Link>
+                    <Link to="/About">ABOUT</Link>
+                </div>
+
+            </div>
         </div>
     );
 };
