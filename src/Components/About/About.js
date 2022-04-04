01bb57e5ff47091be6f4e6e6b28c1c7cdbbcd225
projit-1 <projitmondol@gmail.com>
2022-04-05 04:38:20 +0600

    blogs 3 questions are done and about us page done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h3>about us to knowjjkjkkjhkjhjkh</h3>
-        </div>
+        <div className='about'>
+            <p>
+                Welcome to khulna Car Hat, your number one source for all things product. We're dedicated to giving you the very best of product, with a focus on best looking, userfriendly, and resonable price. </p>
+
+            <p>
+                Founded in 2022 by Projit Mondol, khulna Car Hat has come a long way from its beginnings in Khulna. When Projit Mondolfirst started out, hir passion for  "eco-friendly cleaning products" drove them to  quit day job, do tons of research, etc. so that khulna Car Hat can offer you  "the world's most advanced toothbrush". We now serve customers all over khulna, Bangladesh, and are thrilled that we're able to turn our passion into our own website. </p>
+
+            <p>
+                We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us. </p>
+
+            <p>
+                Sincerely,</p>
+            <p>
+                Projit Mondol</p>
+
+        </div >
     );
 };
 
--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -5,13 +5,13 @@ const Blogs = () => {
     return (
         <div className='question'>
             <div className="question-01">
-                <h2>What is Contex API</h2>
+                <h2>(i)What is Contex API</h2>
                 <p>The React Context API is a process for making a React app to efficiently produce global variables that is able to pass around. This is the alternative to "prop drilling" or moving props from grandparent component to child component to parent component, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
                 <p>
                     Context API is one of the best new features added in version 16.3 of React that allows one to share state across the entire app or part of it lightly and with ease. React.createContext() is very much needed and all you need. It returns receiver and provider just like a consumer and a provider. Generally, Provider is a component that as it's names commonly suggests provides the state to its children this auto-suggestion makes you effective as a developer.</p>
             </div>
             <div className="question-02">
-                <h2>What is Semantic Tag</h2>
+                <h2>(ii)What is Semantic Tag</h2>
                 <p>An important rule in web design and development is the idea of using HTML elements to show what they generally are, rather than how they may be appeared in the browser by default. It is known as the most popularly used semantic HTML.
                     Semantic HTML or semantic markup is HTML actually introduces the meaning to the web page rather than just presentation which is the better solution for SEO as well as code reading for an external code reader.</p>
                 <p>
@@ -19,8 +19,21 @@ const Blogs = () => {
                     If we find out the most uses of semantic HTML tags these will include: Header tags h1 through h6 , blockquote , code, em, p, and ul .  There are many more semantic HTML tags to use as you build a standards-compliant website.</p>
             </div>
             <div className="question-03">
-                <h2>What is Inline-Block, and Inline-Block element</h2>
-                <p>ans</p>
+                <h2>(iii)Difference Between Block Elements And Inline Elements</h2>
+                <p>
+                    There are some significant differences between the inline and block elements of HTML. Which are one of the important areas where a new web developer often gets confused because he/she is unable to know which are inline and block elements. The thinking is very simple one has a closing tag but on the other hand, another has no closing tag.  </p>
+
+                <p> So let us see the differences between the inline and block elements in HTML-5 and the different frequently used inline and block HTML elements.
+                    Block elements consume the entire width available irrespective of their sufficiency. They always start in a new line and have top and bottom margins. It does not contain any other elements next to it.   </p>
+
+                <h5>Examples of Block Elements: </h5>
+                <p> h1 to h6, nav, footer, section, main, article, div, hr, li, ul, ol, p, table, etc. tags are block elements tags. </p>
+
+                <h5>  Examples of Inline Elements: </h5>
+
+                <p> a, strong, em,b, I, q, mark, etc are inline element tags
+                    To be a perfect web developer we need to know all these types of tags or elements clearly. </p>
+
             </div>
         </div>
     );
